b47d3dc500df041cc6f5ba9bd6963b7da925e9c5
Phrancium <tianfrank@gmail.com>
2025-08-30 05:40:30 +0000

    Deploying to gh-pages from @ Phrancium/phrancium.github.io@54b6353a0fbd0e570c280b0b79f7109d96584778 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -400,12 +400,12 @@ ninja.data = [{
               window.location.href = "/books/the_godfather/";
             },},{id: "projects-a-short-quest",
           title: 'A Short Quest',
-          description: "with background image",
+          description: "A story game built using a fine-tuned classifier and an embedded LLM",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+          description: "a silly game about protecting a VIP",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{
